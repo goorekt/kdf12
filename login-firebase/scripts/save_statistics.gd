a634d87eb782825b5944f0c84b40eb3d5b96097c
Kasper Bankler <kasperbankler@gmail.com>
2023-11-28 12:33:53 +0100

    Create load_statistics.gd and save_statistics.gd

--- login-firebase/scripts/save_statistics.gd
+++ login-firebase/scripts/save_statistics.gd
@@ -1,10 +1,8 @@
 extends Control
 
 onready var http : HTTPRequest = $HTTPRequest
-onready var grades : Label = $Container/VBoxContainer/Grades/Label
-onready var tests : Label = $Container/VBoxContainer/Tests/Label
-onready var time : Label = $Container/VBoxContainer/Time/Label
 onready var notification : Label = $Container/Notification
+
 
 var new_profile := false
 var information_sent := false
@@ -12,13 +10,13 @@ var profile := {
 	"grades": {},
 	"tests": {},
 	"time": {}
-} setget set_profile
+}
 
 
 func _ready() -> void:
 	Firebase.get_document("users/%s" % Firebase.user_info.id, http)
 
-#Tilføj HTTPRequest node til scenen og lav signal
+
 func _on_HTTPRequest_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray) -> void:
 	var result_body := JSON.parse(body.get_string_from_ascii()).result as Dictionary
 	match response_code:
@@ -32,9 +30,14 @@ func _on_HTTPRequest_request_completed(result: int, response_code: int, headers:
 				information_sent = false
 			self.profile = result_body.fields
 
-#Bruges til at hente data fra databasen og ændre notes i godot
-func set_profile(value: Dictionary) -> void:
-	profile = value
-	grades.text = profile.grades.stringValue
-	tests.text = profile.tests.intengerValue
-	time.text = profile.time.intengerValue
+
+func _on_ConfirmButton_pressed() -> void:
+	profile.grades = {"arrayValue": {"values": null}} # Erstat med variabel der holder værdien
+	profile.tests = { "integerValue": null } # Erstat med variabel der holder værdien
+	profile.time = { "integerValue": null } # Erstat med variabel der holder værdien
+	match new_profile:
+		true:
+			Firebase.save_document("users?documentId=%s" % Firebase.user_info.id, profile, http)
+		false:
+			Firebase.update_document("users/%s" % Firebase.user_info.id, profile, http)
+	information_sent = true
