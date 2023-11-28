5347b7635bb2d87db7110dbae8dad5a8234d0b6e
Kasper Bankler <kasperbankler@gmail.com>
2023-11-28 12:02:31 +0100

    Create statistics scene

--- login-firebase/scripts/update_database.gd
+++ login-firebase/scripts/update_database.gd
@@ -1,21 +1,17 @@
 extends Control
 
 onready var http : HTTPRequest = $HTTPRequest
-onready var nickname : LineEdit = $Container/VBoxContainer2/Name/LineEdit
-onready var character_class : LineEdit = $Container/VBoxContainer2/Class/LineEdit
+onready var grades : Label = $Container/VBoxContainer/Grades/Label
+onready var tests : Label = $Container/VBoxContainer/Tests/Label
+onready var time : Label = $Container/VBoxContainer/Time/Label
 onready var notification : Label = $Container/Notification
-onready var strength : Slider = $Container/VBoxContainer2/Strength/Slider
-onready var intelligence : Slider = $Container/VBoxContainer2/Intelligence/Slider
-onready var dexterity : Slider = $Container/VBoxContainer2/Dexterity/Slider
 
 var new_profile := false
 var information_sent := false
 var profile := {
-	"nickname": {},
-	"character_class": {},
-	"strength": {},
-	"intelligence": {},
-	"dexterity": {}
+	"grades": {},
+	"tests": {},
+	"time": {}
 } setget set_profile
 
 
@@ -36,28 +32,9 @@ func _on_HTTPRequest_request_completed(result: int, response_code: int, headers:
 				information_sent = false
 			self.profile = result_body.fields
 
-# Tilføj confirm button til scenen for at updatere databasen
-func _on_ConfirmButton_pressed() -> void:
-	if nickname.text.empty() or character_class.text.empty():
-		notification.text = "Please, enter your nickname and class"
-		return
-	profile.nickname = { "stringValue": nickname.text }
-	profile.character_class = { "stringValue": character_class.text }
-	profile.strength = { "integerValue": strength.value }
-	profile.intelligence = { "integerValue": intelligence.value }
-	profile.dexterity = { "integerValue": dexterity.value }
-	match new_profile:
-		true:
-			Firebase.save_document("users?documentId=%s" % Firebase.user_info.id, profile, http)
-		false:
-			Firebase.update_document("users/%s" % Firebase.user_info.id, profile, http)
-	information_sent = true
-
 #Bruges til at hente data fra databasen og ændre notes i godot
 func set_profile(value: Dictionary) -> void:
 	profile = value
-	nickname.text = profile.nickname.stringValue
-	character_class.text = profile.character_class.stringValue
-	strength.value = int(profile.strength.integerValue)
-	intelligence.value = int(profile.intelligence.integerValue)
-	dexterity.value = int(profile.dexterity.integerValue)
+	grades.text = profile.grades.stringValue
+	tests.text = profile.tests.intengerValue
+	time.text = profile.time.intengerValue
